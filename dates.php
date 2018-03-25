a44c81ba8a3631f1134f55985ad593eb976e39ad
Raju Gautam <raju.rachana@gmail.com>
2018-03-25 16:53:19 +0545

    Updated dates.php with description.

--- dates.php
+++ dates.php
@@ -1,9 +1,24 @@
 <?php
+// set default timezone for the date
 date_default_timezone_set('Asia/Kathmandu');
 
-echo date('Y-m-d H:i:s',time());
+// strtotime() function
+// converts english textual dates to timestamps
+echo date('Y-m-d', strtotime("tomorrow")); // displays the tomorrow's date
+
+// date function's second parameter is timestamp
+// default timestamp is current time with time() function
+echo date('Y-m-d H:i:s', time());
 die;
 
+// time function
+// time function returns current timestamp
+// timestamp is number of seconds elapsed from 1970-01-01, linux epoch (an special event)
+// PHP's date function works upon timestamps
+echo time();// returns current time stamps i.e. 1521975179 for 25th March, 2018 16:38
+die;
+
+// show dates in different formats
 echo date('dS F, Y');
 die;
 
